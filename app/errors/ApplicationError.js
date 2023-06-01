deeace6829c63918762fb19a684db152969f4607
risa <risairma21@gmail.com>
2023-06-01 18:50:53 +0700

    feat: add unit testing application

--- app/errors/ApplicationError.js
+++ app/errors/ApplicationError.js
@@ -1,6 +1,16 @@
 class ApplicationError extends Error {
-  get details() {
-    return {};
+  constructor(message, details = null) {
+    super(message);
+    this.name = this.constructor.name;
+
+    Object.defineProperty(this, 'details', {
+      value: details,
+      enumerable: true,
+      writable: true,
+      configurable: true
+    });
+
+    Error.captureStackTrace(this, this.constructor);
   }
 
   toJSON() {
